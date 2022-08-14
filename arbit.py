b89d7ab33c9f620e0a089a52f5919b8ed6d15464
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-08-15 01:24:21 +0200

    first commit

---
+++ arbit.py
@@ -0,0 +1,20 @@
+
+
+import dexTrade
+import exchange
+
+while True:
+
+    #get price of dex
+    #get price of exchage
+
+    # if dex / exchage - 1 > 0.06:
+    #   buy jewel in exchange 
+    #   sell jewel in Dex
+
+    # elif exchage > dex -1 > 0.06 :
+    #   sell jewel in exchange:
+    #   buy jewel in dex
+
+
+

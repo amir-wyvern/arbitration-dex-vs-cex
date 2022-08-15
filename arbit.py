cad3f331a9decd64d33eabd7ea74d66e63c122e7
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-08-15 23:32:23 +0200

    update file

--- arbit.py
+++ arbit.py
@@ -1,10 +1,16 @@
 
+from utils.blockchainAccountBalance import BlockchainBalance
+from utils.exchangeAccountBalance import ExchanceBalance
+from utils.contracts import load_contracs
+from utils.account import Account
+
+from dotenv import load_dotenv
 
 from dex import DexTrade
-# from exchange import CexTrade
+from exchange import Exchange
+
 import logging
-from dexUtils.contracts import load_contracs
-from dexUtils.account import Account
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -23,23 +29,26 @@ class Arbitration:
 
 if __name__ == '__main__':
 
-    list_contracts = load_contracs()
-    account_handler = Account('defiprivatewyvern@79!'.encode())
+    load_dotenv()
+    password = os.getenv('PASSWORD').encode()
+    account_keys = Account(password)
 
+    blockchain_balance = BlockchainBalance(account_keys)
+    exchange_balance = ExchanceBalance(account_keys)
+
+    list_contracts = load_contracs()
+    w3 = Web3(Web3.HTTPProvider('https://api.s0.t.hmny.io'))
     dex = DexTrade(
-        public_address=account_handler.getAddress(),
-        private_address=account_handler.getPri(),
-        base_token_address='0x72cb10c6bfa5624dd07ef608027e366bd690048f',
-        quote_token_address='0xe176ebe47d621b984a73036b9da5d834411ef734',
+        web3= w3,
+        account_keys= account_keys,
+        base_token_address= '0x72cb10c6bfa5624dd07ef608027e366bd690048f',
+        quote_token_address= '0xe176ebe47d621b984a73036b9da5d834411ef734',
         lp_contract= list_contracts['jewel-busd-lp'],
         router_contract= list_contracts['router'],
         )
 
-    resp = dex.buy_base_token(1*10**18)
-    print(resp)
-    # exchange = CexTrade()
-
-
+    exchange = Exchange(account= account_keys)
+    
     #get price of dex
     #get price of exchage
 
@@ -50,9 +59,3 @@ if __name__ == '__main__':
     # elif exchage > dex -1 > 0.06 :
     #   sell jewel in exchange:
     #   buy jewel in dex
-
-
-
-# 0x38ed17390000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000013794c53fe514ac000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000006dc8e55d703c563e4d62cddab12949ec7e4b6e830000000000000000000000000000000000000000000000000000000062fa55eb000000000000000000000000000000000000000000000000000000000000000200000000000000000000000072cb10c6bfa5624dd07ef608027e366bd690048f000000000000000000000000e176ebe47d621b984a73036b9da5d834411ef734
-
-# 1400359904871849472

8818d94f688a56f7b554bab1736192a33f752a88
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-08-16 23:53:44 +0200

    update file

--- exchange.py
+++ exchange.py
@@ -1,8 +1,6 @@
 from mexc_sdk import Market ,Spot
 
 
-
-
 class Exchange:
 
     def __init__(self, account_handler, symbol):
@@ -11,10 +9,10 @@ class Exchange:
         self.symbol = symbol
         self.market = Market(api_key=self.account_handler.api_key, api_secret=self.account_handler.api_secret)
 
-    def buy_token(self):
+    def buy_base_token(self):
         pass
 
-    def sell_token(self):
+    def sell_base_token(self):
         pass
 
     def get_order_book(self ,limit=3):
@@ -24,7 +22,7 @@ class Exchange:
 
     def get_current_price(self):
         
-        resp = client.avg_price(symbol= self.symbol)
+        resp = client.ticker_price(symbol= self.symbol)
         return resp
 
 

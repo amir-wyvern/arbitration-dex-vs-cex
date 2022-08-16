379aa1e9da04cef68a30381110f0d0b7acbe3f5c
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-08-16 02:39:18 +0200

    new filw

---
+++ exchange.py
@@ -0,0 +1,30 @@
+from mexc_sdk import Market ,Spot
+
+
+
+
+class Exchange:
+
+    def __init__(self, account_handler, symbol):
+        
+        self.account_handler = account_handler
+        self.symbol = symbol
+        self.market = Market(api_key=self.account_handler.api_key, api_secret=self.account_handler.api_secret)
+
+    def buy_token(self):
+        pass
+
+    def sell_token(self):
+        pass
+
+    def get_order_book(self ,limit=3):
+
+        resp = client.depth(symbol= self.symbol ,options={ 'limit': limit })
+        return resp
+
+    def get_current_price(self):
+        
+        resp = client.avg_price(symbol= self.symbol)
+        return resp
+
+

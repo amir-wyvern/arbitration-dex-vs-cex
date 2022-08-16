66ef955a45951cf9097f983ea9ef3454228ff0f1
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-08-16 02:50:41 +0200

    update file

--- utils/contracts.py
+++ utils/contracts.py
@@ -3,7 +3,7 @@ import glob
 
 def load_contracs():
     ls_contracts = {}
-    for _file in glob.glob('./dexUtils/contract-*.json'):
+    for _file in glob.glob('./contracts/contract-*.json'):
         with open(_file ,'r') as fi :
             fileData = json.load(fi)
             ls_contracts[fileData['name']] = fileData
